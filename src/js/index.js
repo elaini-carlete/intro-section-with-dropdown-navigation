bb546369639be135da4c2058e29732ab44e32ddc
Elaini Carlete <elainicarletethomazelli3@gmail.com>
2023-11-07 10:05:36 -0300

    adicionado os arquivos

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const iconeMenuAbrir = document.querySelector(".menu-hamburguer");
+const iconeMenuFechar = document.querySelector(".menu-close");
+const menuPrincipal = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+
+iconeMenuAbrir.addEventListener("click", abrirMenu);
+iconeMenuFechar.addEventListener("click", fecharMenu);
+
+function abrirMenu () {
+    menuPrincipal.classList.add("ativo");
+    overlay.classList.add("ativo");
+}
+
+function fecharMenu () {
+    menuPrincipal.classList.remove("ativo");
+    overlay.classList.remove("ativo");
+}
